0a32019163b731392e11728fc88d7ba9b28058c1
Dener Navarro <dener.navarro@gmail.com>
2023-08-28 19:01:02 -0300

    commit inicial

---
+++ src/TarefaWrapper.java
@@ -0,0 +1,9 @@
+public class TarefaWrapper {
+    public static void main(String[] args) {
+        int num1 = 15;
+        Integer num2 = 15;
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
