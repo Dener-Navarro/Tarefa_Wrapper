a442b95efb4640fce76f3777ad2285ded5bccebb
Dener Navarro <dener.navarro@gmail.com>
2023-08-30 12:28:02 -0300

    alterando para autoboxing

--- src/TarefaWrapper.java
+++ src/TarefaWrapper.java
@@ -1,9 +1,11 @@
 public class TarefaWrapper {
     public static void main(String[] args) {
         int num1 = 15;
-        Integer num2 = 15;
+        Integer num2 = num1;
+
 
         System.out.println(num1);
         System.out.println(num2);
+
     }
 }
